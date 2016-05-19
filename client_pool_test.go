a2051e2198c9aa5c36951f5f9cc0d00588330450
Friedrich Große <friedrich.grosse@classmarkets.com>
2016-05-19 08:52:19 +0200

    Add more tests for the ClientPool decorator

--- client_pool_test.go
+++ client_pool_test.go
@@ -1,11 +1,11 @@
 package cmhttp_test
 
 import (
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
-	"math/rand"
 
 	"github.com/bitly/go-hostpool"
 	"github.com/classmarkets/cmhttp"
@@ -16,9 +16,6 @@ func init() {
 }
 
 func TestClientPool(t *testing.T) {
-	decayDuration := 1 * time.Second // very short interval just for the test
-	valueCalculator := new(hostpool.LinearEpsilonValueCalculator)
-
 	handlers := map[string]http.Handler{
 		"fast": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(10 * time.Millisecond)
@@ -31,7 +28,84 @@ func TestClientPool(t *testing.T) {
 		}),
 	}
 
-	stats := make(map[string]int)
+	stats := runClientPoolTest(t, 100, handlers)
+
+	for name := range handlers {
+		t.Logf("%s handler received %d requests", name, stats[name])
+		if stats[name] == 0 {
+			t.Errorf("Each handler should have gotten at least one request but %s has none (0)", name)
+		}
+	}
+
+	if stats["slow"] > stats["mediocre"] {
+		t.Error("Slow handler received more requests than the mediocre handler")
+	}
+
+	if stats["mediocre"] > stats["fast"] {
+		t.Error("Mediocre handler received more requests than the fast handler")
+	}
+}
+
+func TestClientPool_WhenSomeHostsFail(t *testing.T) {
+	handlers := map[string]http.Handler{
+		"ok": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(10 * time.Millisecond)
+		}),
+		"fail1": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}),
+		"fail2": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}),
+	}
+
+	stats := runClientPoolTest(t, 100, handlers)
+	for name := range handlers {
+		t.Logf("%s handler received %d requests", name, stats[name])
+		if stats[name] == 0 {
+			t.Errorf("Each handler should have gotten at least one request but %s has none (0)", name)
+		}
+	}
+
+	if stats["fail1"] > stats["ok"] {
+		t.Error("Failure handler 1 received more requests than the ok handler")
+	}
+
+	if stats["fail2"] > stats["ok"] {
+		t.Error("Failure handler 2 received more requests than the ok handler")
+	}
+}
+
+func TestClientPool_WhenAllHostsFail(t *testing.T) {
+	handlers := map[string]http.Handler{
+		"s1": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}),
+		"s2": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}),
+		"s3": http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}),
+	}
+
+	n := 100
+	stats := runClientPoolTest(t, n, handlers)
+	var total int
+	for _, s := range stats {
+		total += s
+	}
+
+	if total != n {
+		t.Errorf("Total number of made requests == %d, want %d", total, n)
+	}
+}
+
+func runClientPoolTest(t *testing.T, n int, handlers map[string]http.Handler) (stats map[string]int) {
+	decayDuration := 1 * time.Second // very short interval just for the test
+	valueCalculator := new(hostpool.LinearEpsilonValueCalculator)
+
+	stats = make(map[string]int)
 	var hosts []string
 	for name := range handlers {
 		name := name // redefine in this scope so it doesn't get overwritten in the next loop iteration
@@ -47,7 +121,7 @@ func TestClientPool(t *testing.T) {
 		cmhttp.ClientPool(hosts, decayDuration, valueCalculator),
 	)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < n; i++ {
 		req, _ := http.NewRequest("GET", "/", nil)
 		resp, err := c.Do(req)
 		if err != nil {
@@ -56,18 +130,5 @@ func TestClientPool(t *testing.T) {
 		resp.Body.Close()
 	}
 
-	for name := range handlers {
-		t.Logf("%s handler received %d requests", name, stats[name])
-		if stats[name] == 0 {
-			t.Errorf("Each handler should have gotten at least one request but %s has none (0)", name)
-		}
-	}
-
-	if stats["slow"] > stats["mediocre"] {
-		t.Errorf("Slow handler received more requests than the mediocre handler")
-	}
-
-	if stats["mediocre"] > stats["fast"] {
-		t.Errorf("Mediocre handler received more requests than the fast handler")
-	}
+	return stats
 }
